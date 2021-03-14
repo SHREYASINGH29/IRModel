4e2d02c05ac8b5fdbe414d3891c8ee3f09129cf9
SHREYASINGH29 <296shreya@gmail.com>
2021-03-14 15:58:31 +0530

    Handle alloca instruction

--- LLVMIRInstModel/IRModel.cpp
+++ LLVMIRInstModel/IRModel.cpp
@@ -2,6 +2,10 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/InstIterator.h"
+#include <iostream>
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
@@ -13,13 +17,20 @@ class IRModelPass : public FunctionPass {
        public:
 	static char ID;
 	IRModelPass() : FunctionPass(ID) {}
-
+	void handleAlloca(Instruction &I){
+		if(auto *AI = dyn_cast<AllocaInst>(&I)){
+			std::cout<<AI->getName().str()<< " = new memloc"<<std::endl;
+		}
+	}
 	bool runOnFunction(Function &F) override {
+		for(inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I){
+			handleAlloca(*I);
+		}
 		return false;
 	}
 };
 }  // namespace
-
+	
 char IRModelPass::ID = 0;
 static RegisterPass<IRModelPass> X(
     "irmodel",			      // the option name -> -mba
